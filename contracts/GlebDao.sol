bf82f0bf6948c6d67f5407ec735b4ff9bb1e2c50
dzahbarov <dzahbarovvladimir@gmail.com>
2022-12-31 03:24:09 +0300

    upd

--- contracts/GlebDao.sol
+++ contracts/GlebDao.sol
@@ -9,24 +9,44 @@ contract GlebDao {
     uint256 constant private PROPOSAL_TTL = 3 days;
     uint8 constant private MAX_PROPOSALS = 3;
 
-    ERC20Votes public token;
+    ERC20Votes private token;
 
+    Proposal[MAX_PROPOSALS] private proposals;
+
+    mapping(bytes32 => uint8) proposalToIdx;
+ 
     enum ProposalState {
+        Absent,
         Accepted,
         Rejected,
-        Pending,
+        Active,
         Expired
     }
 
     struct Proposal {
         ProposalState state;
-        uint256 createdAt;
+        uint256 createdAtBlock;
         uint256 expiredAt;
-        
+        uint256 agree;
+        uint256 disagree;
     }
+
+    struct Vote {
+        bool agree;
+        uint256 weight;
+    }
+
+
+    event CreatedProposal(bytes32 indexed proposalId, uint256 expiration, uint256 startBlock,  address indexed author);
+    event FinishedProposal(bytes32 indexed proposalId, ProposalState state);
 
     modifier voteAllowed {
         require(token.balanceOf(msg.sender) > 0, "GlebDao: Not enough balance");
+        _;
+    }
+
+    modifier proposalUnique(bytes32 _proposalId) {
+        require(proposals[proposalToIdx[_proposalId]].state == ProposalState.Absent, "GlebDao: Proposal already exists");
         _;
     }
 
@@ -35,12 +55,55 @@ contract GlebDao {
         token = ERC20Votes(_tokenAddress);
     }
 
+    function createProposal(bytes32 _proposalId) external voteAllowed proposalUnique(_proposalId) {
+        uint8 idx = MAX_PROPOSALS;
+        for (uint8 i = 0; i < MAX_PROPOSALS; i++) {
+            if (proposals[i].state == ProposalState.Absent || proposals[i].expiredAt >= block.timestamp) {
+                idx = i;
+                break;
+            }
+        }
+
+        require(idx != MAX_PROPOSALS, "GlebDao: Max proposals number reached");
+
+        proposalToIdx[_proposalId] = idx;
+
+        proposals[idx] = Proposal({
+            state: ProposalState.Active,
+            createdAtBlock: block.number,
+            expiredAt: block.timestamp + PROPOSAL_TTL,
+            agree: 0,
+            disagree: 0
+            // idx: idx
+        });
+
+        emit CreatedProposal(_proposalId, block.timestamp + PROPOSAL_TTL, block.number, msg.sender);
+    }
+
+    // function vote(bytes32 _proposalId, uint256 weight, bool agree) {
+// 
+    // }
 
 
+    function _checkState(bytes32 _proposalId) internal {
+        Proposal storage proposal = proposals[proposalToIdx[_proposalId]];
+        uint256 totalSupply = token.getPastTotalSupply(proposal.createdAtBlock);
 
+        if (proposal.agree > totalSupply / 2) {
+            proposal.expiredAt = 0;
+            emit FinishedProposal(_proposalId, ProposalState.Accepted);
+        } else if (proposal.disagree > totalSupply / 2) {
+            proposal.expiredAt = 0;
+            emit FinishedProposal(_proposalId, ProposalState.Rejected);
+        } else if (proposal.expiredAt >= block.timestamp) {
+            emit FinishedProposal(_proposalId, ProposalState.Expired);
+        }
+    }
 
-
-    // event ProposalFinished()
-
-    
+    function getProposal(bytes32 _proposalId) internal view returns (Proposal storage) {
+        Proposal storage proposal = proposals[proposalToIdx[_proposalId]];
+        require(proposal.state != ProposalState.Absent, "Proposal doesn't exists");
+        require(proposal.expiredAt < block.timestamp, "Proposal expired");
+        return proposal;
+    }
 }
